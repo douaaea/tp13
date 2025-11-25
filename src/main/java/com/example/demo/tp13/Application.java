41956606e91d26f33fb95ae4949c5d1fcb00fc98
douaaea <amezianedouaae@gmail.com>
2025-11-25 13:08:21 +0100

    commit

--- src/main/java/com/example/demo/tp13/Application.java
+++ src/main/java/com/example/demo/tp13/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tp13Application {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Tp13Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
